58d884866efde753e5d0e5aea0b7e31fb509c6f0
morelmiles <morelmiles@gmail.com>
2020-05-05 12:01:01 +0300

    Added a user model and connected to the db

--- server.js
+++ server.js
@@ -1,30 +1,49 @@
-const express= require('express');
-const mongoose= require('mongoose');
-const bodyParser= require('body-parser');
-const port=8000;
-const app= express();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const User = require("./models/User");
+
+//Coonect to the local database
+const url = "mongodb://localhost:27017/userData";
+mongoose.connect(url, { useNewUrlParser: true });
+
+//Check if the db has connected
+const db = mongoose.connection;
+db.once("open ", (_conn) => {
+  console.log("Database has connected ", url);
+});
+db.on("error", (err) => {
+  console.log("Connection error ", err);
+});
+//Initialize express
+const app = express();
 
 app.use(bodyParser.json());
 
-app.listen(port, ()=>{
-	console.log(`server is listening on port:${port}`)
-})
-
 // CREATE
-app.post('/users',(req,res)=>{
+app.post("/users", (req, res) => {
   // User.create()
-})
+});
 
-app.route('/users/:id')
-// READ
-.get((req,res)=>{
-  // User.findById()
-})
-// UPDATE
-.put((req,res)=>{
-  // User.findByIdAndUpdate()
-})
-// DELETE
-.delete((req,res)=>{
-  // User.findByIdAndDelete()
-})
+app
+  .route("/users/:id")
+  // READ
+  .get((req, res) => {
+    // User.findById()
+  })
+  // UPDATE
+  .put((req, res) => {
+    // User.findByIdAndUpdate()
+  })
+  // DELETE
+  .delete((req, res) => {
+    // User.findByIdAndDelete()
+  });
+
+//Port
+const port = 8000;
+
+//Listen for the open port
+app.listen(port, () => {
+  console.log(`server is listening on port:${port}`);
+});
